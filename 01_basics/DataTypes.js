36505b5b557fcf172608bb844cf398be40d57eb9
Dharmender Chaturvedi <dharmender1383@gmail.com>
2024-07-06 16:32:20 +0000

    DataTypes

---
+++ 01_basics/DataTypes.js
@@ -0,0 +1,24 @@
+"use strict";// treat all js code as newer version
+
+// alert (3+3) // we are using nodejs,not browser
+console.log(3+3); console.log("Dharmender");
+
+let name = "dharm"
+let age = 22
+let isLoggedIn = false
+let state;
+
+//number => 2 to 53
+//bigInt
+//string =>" "
+//boolean => true/false
+//null => standalone value
+//undefined => 
+//symbol
+
+//object
+console.log(typeof "dharam")
+console.log(typeof age)
+console.log(typeof null)
+console.log(typeof undefined)
+
